4c1c966f4d0b00a5e4f78edd92b36609043a8b63
OkaMana <95357279+OkaMana@users.noreply.github.com>
2022-01-30 02:43:15 +0900

    Update script.js

--- clock/script.js
+++ clock/script.js
@@ -2,10 +2,10 @@ const clock = () => {
   // 現在の日時・時刻の情報を取得
   const d = new Date();
   
-　let year = d.toLocaleString( "ja-JP-u-ca-japanese" ,{ era : "long" });
+　let data = d.toLocaleString( "ja-JP-u-ca-japanese" ,{ era : "long" });
 
   // 日付・時刻の文字列を作成
-  let today = `今日は${year}だよ〜`;
+  let today = `今日は ${data} だよ〜`;
 
   // 文字列を出力
   document.querySelector(".clock-date").innerText = today;
